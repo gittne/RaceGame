841e354a59ee2260e8d7697e379209d9e3bcf4e5
gittne <gustav.eriksson6@gmail.com>
2023-08-08 21:53:09 +0200

    Added steering animation to kart 2

--- Assets/Gustav/SCR_WheelController.cs
+++ Assets/Gustav/SCR_WheelController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class SCR_WheelController : MonoBehaviour
 {
+    //Wheel related values
+    [Header("Wheel Related Fields")]
     [SerializeField] GameObject[] wheelsToRotate;
     [SerializeField] float rotationSpeed;
+
+    //Steering axel related values
+    [Header("Steering Axel Fields")]
+    [SerializeField] GameObject steeringAxel;
+    [SerializeField] Animator axelAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +23,38 @@ public class SCR_WheelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateWheels();
+        RotateAxel();
+    }
+
+    void RotateWheels()
+    {
         float gasInput = Input.GetAxisRaw("Accelerate");
 
         foreach (var wheel in wheelsToRotate)
         {
             wheel.transform.Rotate(Time.deltaTime * gasInput * rotationSpeed, 0, 0, Space.Self);
+        }
+    }
+
+    void RotateAxel()
+    {
+        float steeringInput = Input.GetAxisRaw("Horizontal");
+
+        if (steeringInput > 0)
+        {
+            axelAnimator.SetBool("goingLeft", false);
+            axelAnimator.SetBool("goingRight", true);
+        }
+        else if(steeringInput < 0)
+        {
+            axelAnimator.SetBool("goingRight", false);
+            axelAnimator.SetBool("goingLeft", true);
+        }
+        else
+        {
+            axelAnimator.SetBool("goingLeft", false);
+            axelAnimator.SetBool("goingRight", false);
         }
     }
 }
